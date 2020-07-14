31ffa6393310500aa1ff457c361adba0291d4553
Timofey Marenko <nkrayner@gmail.com>
2020-07-14 15:02:07 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Twitch Chat Anti-Ban
+This extension automatically opens a proxy chat to channels where you are banned.
---
+++ src/content.js
@@ -0,0 +1,10 @@
+$(document).ready(() => {
+    window.setInterval(function () {
+        if ($('#open-chat').length < 1 && $('#proxy_chat').length < 1) {
+			let channel = (location.href.indexOf('popout') > -1) ? location.href.split('/')[4] : location.href.split('/')[3];
+			let url = `https://nightdev.com/hosted/obschat/?theme=bttv_dark&channel=${channel}&fade=false&bot_activity=true&prevent_clipping=true`;
+			let div_data = `<iframe name="proxy_chat" id="proxy_chat" src="${url}"  style="width: 100%;"></iframe>`;
+			$('.chat-room__content > .scrollable-area').html(div_data);
+        }
+    }, 250);
+});
