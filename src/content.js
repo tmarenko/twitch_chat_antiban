6af709c8483f5c006c426b3ac00f8dec50caf175
tmarenko <nkrayner@gmail.com>
2020-10-11 16:33:10 +0300

    nested chat search

--- src/content.js
+++ src/content.js
@@ -4,7 +4,7 @@ $(document).ready(() => {
 			let channel = (location.href.indexOf('popout') > -1) ? location.href.split('/')[4] : location.href.split('/')[3];
 			let url = `https://nightdev.com/hosted/obschat/?theme=bttv_dark&channel=${channel}&fade=false&bot_activity=true&prevent_clipping=true`;
 			let div_data = `<iframe name="proxy_chat" id="proxy_chat" src="${url}"  style="width: 100%;"></iframe>`;
-			$('.chat-room__content > .scrollable-area').html(div_data);
+			$('.chat-room__content .scrollable-area').html(div_data);
         }
     }, 250);
 });
