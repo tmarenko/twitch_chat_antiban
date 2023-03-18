cb75e3379819579973b00bdb02efc13cb0083fd3
tmarenko <nkrayner@gmail.com>
2023-03-18 17:56:36 +0300

    load proxy chat by "you're banned" message

--- src/content.js
+++ src/content.js
@@ -1,13 +1,26 @@
+function exists(selector) {
+    return document.querySelectorAll(selector).length > 0;
+}
+
+function isBanned() {
+	return [
+		'[data-test-selector="banned-user-message"]',
+		'[data-test-selector="request-unban-link"]',
+		'[data-test-selector="cooldown-text"]'
+	].some(exists);
+}
+
 $(document).ready(() => {
     window.setInterval(function () {
-        if ($('.chat-room__content .scrollable-area').length < 1 && $('#proxy_chat').length < 1) {
-			let channel = (location.href.indexOf('popout') > -1) ? location.href.split('/')[4] : location.href.split('/')[3];
-			let url = `https://nightdev.com/hosted/obschat/?theme=bttv_dark&channel=${channel}&fade=false&bot_activity=true&prevent_clipping=true`;
-			let div_data = `<iframe name="proxy_chat" id="proxy_chat" src="${url}"  style="width: 100%; height: 100%;"></iframe>`;
-			let chat_el = $('.chat-room__content').children().first();
-			chat_el.removeClass();
-			chat_el.addClass("chat-list--default scrollable-area");
-			chat_el.html(div_data);
+        if (isBanned() && !exists('#proxy-chat')) {
+            console.log("Loading proxy chat");
+            let channel = (location.href.indexOf('popout') > -1) ? location.href.split('/')[4] : location.href.split('/')[3];
+            let url = `https://nightdev.com/hosted/obschat/?theme=bttv_dark&channel=${channel}&fade=false&bot_activity=true&prevent_clipping=true`;
+            let div_data = `<iframe name="proxy-chat" id="proxy-chat" src="${url}"  style="width: 100%; height: 100%;"></iframe>`;
+            let chat_el = $('.chat-room__content').children().first();
+            chat_el.removeClass();
+            chat_el.addClass("chat-list--default scrollable-area");
+            chat_el.html(div_data);
         }
-    }, 250);
+    }, 1000);
 });
