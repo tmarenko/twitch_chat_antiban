f53a2e117815c46e1c4dfca7aa332b3de6982c62
tmarenko <nkrayner@gmail.com>
2025-01-18 12:05:20 +0200

    handle undefined channels

--- src/scripts/content.js
+++ src/scripts/content.js
@@ -25,38 +25,49 @@ function isStreamBanned() {
 function getChannel() {
     // support for embedded location
     const search = location.search.substring(1); // Remove the '?' at the start
-    const params = search.split('&').reduce((acc, current) => {
-        const [key, value] = current.split('=');
-        acc[key] = value;
-        return acc;
-    }, {});
+    if (search) {
+        const params = search.split('&').reduce((acc, current) => {
+            if (!current) return acc;
+            const [key, value] = current.split('=');
+            if (key) acc[key] = value ?? '';
+            return acc;
+        }, {});
 
-    if (params.channel) {
-        return decodeURIComponent(params.channel);
+        if (params.channel) {
+            const channel = decodeURIComponent(params.channel);
+            return channel || null;
+        }
     }
 
     // support for twitch.tv location
-    return location.pathname.split('/').filter(
+    const channel = location.pathname.split('/').filter(
         segment => segment && segment !== 'popout' && segment !== 'chat' && segment !== 'embed'
     ).shift();
+    
+    return channel || null;
 }
 
 
 $(function () {
     window.setInterval(function () {
+        const currentChannel = getChannel();
+        if (!currentChannel) {
+            return;
+        }
+
         if (isBanned() && !exists('#anti-ban-chat')) {
             console.log("Twitch Anti-Ban: loading proxy chat");
             ProxyChat.initChat();
-            ProxyChat.connect(getChannel());
+            ProxyChat.connect(currentChannel);
         }
 
         if (isStreamBanned() && !exists('#anti-ban-stream')) {
             console.log("Twitch Anti-Ban: loading proxy stream");
             ProxyStream.restoreOriginalPlayer();
-            ProxyStream.initStream(getChannel());
+            ProxyStream.initStream(currentChannel);
         }
 
-        if (ProxyStream.channel && ProxyStream.channel !== getChannel()) {
+        if (ProxyStream.channel && ProxyStream.channel !== currentChannel) {
             console.log("Twitch Anti-Ban: restoring original player");
             ProxyStream.restoreOriginalPlayer();
         }
